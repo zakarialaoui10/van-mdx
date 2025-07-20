cc0841ca97b361ff111bcb620cc0d4f99b06540f
zakarialaoui10 <zakarialaoui10@gmail.com>
2025-07-20 13:04:21 +0100

    fix example

--- examples/hello-world/src/App.js
+++ examples/hello-world/src/App.js
@@ -1,7 +1,6 @@
 import van from "vanjs-core";
 import "./App.css";
-// import Article from "./content/Article.mdx"
-import Article from "./content/a2.mdx"
+import Article from "./content/Article.mdx"
 
 export const App = () => {
   return Article({border_color : "green"})
